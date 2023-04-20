c57842a79f0636bdc49a1a7a84124be886a53b22
Natalia-GP <nataliag86@yahoo.es>
2023-04-20 11:36:11 +0200

    canvas responsive

--- .vscode/game.js
+++ .vscode/game.js
@@ -1,37 +1,35 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+//VARIABLES
+let canvasSize;
+let elementsSize;
 
-function startGame() {
-  let canvasSize;
+//EVENTS
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
+//FUNCTIONS
+
+function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
-    canvasSize = window.innerWidth * 0.8;
+    canvasSize = window.innerWidth * 0.7;
   } else {
-    canvasSize = window.innerHeight * 0.8;
+    canvasSize = window.innerHeight * 0.7;
   }
 
   canvas.setAttribute('width', canvasSize);
   canvas.setAttribute('height', canvasSize);
-
-  const elementsSize = canvasSize / 10;
-
+  elementsSize = canvasSize / 10;
+  startGame();
+}
+function startGame() {
   console.log({ elementsSize, canvasSize });
 
   game.font = elementsSize + 'px Verdana';
   game.textAlign = 'end';
+
   for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis['X'], elementsSize * i, elementsSize);
+    game.fillText(emojis['X'], elementsSize, elementsSize * i);
   }
-
-  //game.fillRect(0, 50, 100, 100);
-  //game.clearRect(50, 50, 50, 50); //.clearReact(x:number, y:number, w:number, h:number)
-  //game.clearRect();
-  //game.clearRect(0,0,50,50);
-
-  //game.font = '25px Verdana';
-  //game.fillStyle = 'purple';
-  //game.textAlign = 'start';
-  //game.fillText('Hola', 100, 100);
 }
